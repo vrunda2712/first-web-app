a759f75f62c2f2ea68931d5d81cbec4953c122bb
vrunda2712 <angy.v27@gmail.com>
2017-06-02 12:47:32 -0400

    final commit

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
 get '/home' do
   erb :index
 end
@@ -12,4 +16,9 @@ get '/about_me' do
   @skills = ['git', 'HTML', 'CSS', 'Ruby']
   @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['http://www.google.com', 'http://www.facebook.com', 'https://ruby-doc.org']
+  erb :favourites
 end
