ecb86b6319153d142c66c57f7a84a4e6af0f0ba1
vrunda2712 <angy.v27@gmail.com>
2017-06-02 12:33:17 -0400

    about_me file added

--- server.rb
+++ server.rb
@@ -7,3 +7,9 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
